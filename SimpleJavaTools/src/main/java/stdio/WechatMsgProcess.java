c46d86a3a8a0d99f8cbd629db5a413682bb34bc3
Zexi Jesse Zhuang <zhzexi@gmail.com>
2016-08-01 11:29:40 -0700

    add map in wechat, sample in resources

--- SimpleJavaTools/src/main/java/stdio/WechatMsgProcess.java
+++ SimpleJavaTools/src/main/java/stdio/WechatMsgProcess.java
@@ -6,11 +6,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A utility class for processing wechat message histories.
  */
 public class WechatMsgProcess {
+
+  private static Map<String, List<String>> waterByName = new HashMap<>();
+
   public static void skipLines(String inFile) {
     try {
       BufferedReader br = Files.newBufferedReader(Paths.get(inFile));
@@ -18,14 +25,32 @@ public class WechatMsgProcess {
           StandardOpenOption.CREATE);
       String line;
       boolean isUserLine = true;
+      String username = "default blank user";
+      bw.write("All watering contents:");
+      bw.newLine();
       while ((line = br.readLine()) != null) {
         // skip empty lines and date lines
         if (line.length() == 0 || line.startsWith("—————")) continue;
         if (!isUserLine) {
+          if (!waterByName.containsKey(username))
+            waterByName.put(username, new ArrayList<String>());
+          waterByName.get(username).add(line);
           bw.write(line);
           bw.newLine();
-        }
+        } else username = line.substring(0, line.indexOf(" "));
         isUserLine = !isUserLine;
+      }
+      bw.write("\n\n\n");
+      bw.write("All watering contents by username:");
+      bw.newLine();
+      for (String user : waterByName.keySet()) {
+        bw.write(user);
+        bw.newLine();
+        List<String> messages = waterByName.get(user);
+        for (String msg : messages) {
+          bw.write("  " + msg);
+          bw.newLine();
+        }
       }
       br.close();
       bw.close();
